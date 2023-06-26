6afc74ba345c4a1f10f99f06fefdce33dd615c8d
Melihcan Cilek <melihcanclk@hotmail.com>
2023-06-26 18:28:35 +0300

    Access Modifier Readme

--- Java102/src/firstPackage/Main.java
+++ Java102/src/firstPackage/Main.java
@@ -1,7 +1,5 @@
 package firstPackage;
 
-import firstPackage.FirstClass;
-
 public class Main {
     public static void main(String[] args) {
         FirstClass firstClass = new FirstClass("Hello World");
