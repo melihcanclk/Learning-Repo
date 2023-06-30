642dbf36115b8c02cbbba332e837e6744445613a
Melihcan Cilek <melihcanclk@hotmail.com>
2023-06-30 12:02:35 +0300

    PreparedStatement example for jdbc

--- Java102/jdbc-postgresql/src/Main.java
+++ Java102/jdbc-postgresql/src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -11,14 +13,24 @@ public class Main {
             * student name with varchar(50)
             * class that a student takes which is varchar(5)
             * */
-            ResultSet resultSet = connection.connect("SELECT * FROM university");
-            while(resultSet.next()){
-                System.out.println(resultSet.getInt("student_id"));
-                System.out.println(resultSet.getString("student_name"));
-                System.out.println(resultSet.getString("class"));
-            }
+//            printDB(connection);
+//            String addVal = "INSERT INTO university (student_name, class) VALUES ('Mertcan Cilek', 'FIZ1');";
+//            System.out.println(connection.update(addVal));
+//            printDB(connection);
+//            connection.preparedStatementExample();
+            printDB(connection);
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static void printDB( PostgreConnect connection) throws SQLException {
+        ResultSet resultSet = connection.execute("SELECT * FROM university");
+        while(resultSet.next()){
+            System.out.println(resultSet.getInt("student_id"));
+            System.out.println(resultSet.getString("student_name"));
+            System.out.println(resultSet.getString("class"));
         }
     }
 }
--- Java102/jdbc-postgresql/src/PostgreConnect.java
+++ Java102/jdbc-postgresql/src/PostgreConnect.java
@@ -2,6 +2,7 @@ import java.sql.*;
 
 public class PostgreConnect{
     private Statement st = null;
+    private Connection connection = null;
 
     PostgreConnect(){
         try {
@@ -9,14 +10,30 @@ public class PostgreConnect{
             String db_url = env.getUrl();
             String db_username = env.getUsername();
             String db_password = env.getPassword();
-            Connection connection = DriverManager.getConnection(db_url, db_username, db_password);
+            connection = DriverManager.getConnection(db_url, db_username, db_password);
             st = connection.createStatement();
         } catch (SQLException e){
             System.out.println(e.getMessage());
         }
     }
 
-    public ResultSet connect(String query) throws SQLException{
+    public ResultSet execute(String query) throws SQLException{
         return st.executeQuery(query);
+    }
+
+    public int update(String query) throws SQLException {
+        return st.executeUpdate(query);
+    }
+
+    public void preparedStatementExample(){
+        String preparedSt = "INSERT INTO university(student_name, class) VALUES(?,?);";
+        try {
+            PreparedStatement prst = connection.prepareStatement(preparedSt);
+            prst.setString(1, "Damla");
+            prst.setString(2, "DIN1");
+            prst.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
