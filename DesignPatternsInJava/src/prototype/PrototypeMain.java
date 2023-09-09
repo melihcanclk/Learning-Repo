3f4f90e7451158c3b881a7b6368ac6a9d024f931
Melihcan Cilek <melihcanclk@hotmail.com>
2023-09-09 11:56:29 +0300

    feat(DesignPatternsInJava): PrototypeMain Link add as source

--- DesignPatternsInJava/src/prototype/PrototypeMain.java
+++ DesignPatternsInJava/src/prototype/PrototypeMain.java
@@ -4,6 +4,7 @@ package prototype;
  * We're going to create an abstract class Shape and concrete classes extending the Shape class.
  * A class ShapeCache is defined as a next step which stores shape objects in a Hashtable and returns their clone when requested.
  * PrototypPatternDemo, our demo class will use ShapeCache class to get a Shape object.
+ * <a href="https://www.tutorialspoint.com/design_pattern/prototype_pattern.htm">Prototype Pattern</a>
  */
 public class PrototypeMain {
     public static void main(String[] args) {
