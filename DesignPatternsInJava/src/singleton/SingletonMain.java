742fd80e3119caf3ef8c1481d816e0a43a882d95
Melihcan Cilek <melihcanclk@hotmail.com>
2023-09-09 11:58:19 +0300

    feat(DesignPatternsInJava): SingletonMain Link add as source

--- DesignPatternsInJava/src/singleton/SingletonMain.java
+++ DesignPatternsInJava/src/singleton/SingletonMain.java
@@ -1,5 +1,11 @@
 package singleton;
 
+/**
+ * We're going to create a SingleObject class. SingleObject class have its constructor as private and have a static instance of itself.
+ * SingleObject class provides a static method to get its static instance to outside world. SingletonPatternDemo, our demo
+ * class will use SingleObject class to get a SingleObject object.
+ * <a href="https://www.tutorialspoint.com/design_pattern/singleton_pattern.htm">Singleton Pattern</a>
+ */
 public class SingletonMain {
     public static void main(String[] args) {
         //illegal construct
