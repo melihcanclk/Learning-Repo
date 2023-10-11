8fc18972e204e0c89b561b37db72ea6c69f784b9
Melihcan Cilek <melihcanclk@hotmail.com>
2023-10-11 18:36:08 +0300

    feat(test): Test getting id method in Repository layer and use TestEntityManager to create entity in database and persist it using that manager.

--- DepartmentApplication/src/main/java/com/melihcanclk/DepartmentApplication/entities/Department.java
+++ DepartmentApplication/src/main/java/com/melihcanclk/DepartmentApplication/entities/Department.java
@@ -4,12 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Data
